fc8b72ac6a713649a1e02b970c309ebd2a86f245
Patricia Johansson <pat.joh.2025@gmail.com>
2025-10-08 16:19:06 +0200

    Add jsdoc plugin to linter

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -3,9 +3,21 @@
 import eslint from '@eslint/js'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import jsdoc from 'eslint-plugin-jsdoc'
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strict,
-  tseslint.configs.stylistic
+  tseslint.configs.stylistic,
+  [
+		{
+			files: ["**/*.ts"],
+			plugins: {
+				jsdoc: jsdoc,
+			},
+			rules: {
+				'jsdoc/require-description': 'warn'
+			},
+		},
+	]
 )
