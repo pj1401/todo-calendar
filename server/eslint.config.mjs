a1c53fc626a811a033b4248cda926a4ba7798d8e
Patricia Johansson <142814895+pj1401@users.noreply.github.com>
2025-10-09 11:09:57 +0200

    Feature/server start (#1)
    * Add jsdoc plugin to linter
    
    * Add Server start
    
    * Add README.md
    
    * Update linter

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -3,9 +3,26 @@
 import eslint from '@eslint/js'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import jsdoc from 'eslint-plugin-jsdoc'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strict,
-  tseslint.configs.stylistic
+  tseslint.configs.stylistic,
+  stylistic.configs.recommended,
+  [
+    {
+      files: ['**/*.ts'],
+      plugins: {
+        jsdoc: jsdoc
+      },
+      rules: {
+        '@stylistic/comma-dangle': ['error', 'never'],
+        '@stylistic/space-before-function-paren': ['error', 'always'],
+        '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+        'jsdoc/require-description': 'warn'
+      }
+    }
+  ]
 )
