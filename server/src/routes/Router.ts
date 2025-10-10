bd28b7e8bc9dfe370dc38075df1bd795958bae39
Patricia Johansson <pat.joh.2025@gmail.com>
2025-10-10 16:15:51 +0200

    Add jsdoc

--- server/src/routes/Router.ts
+++ server/src/routes/Router.ts
@@ -1,8 +1,15 @@
 // User-land modules.
 import express from 'express'
 
+/**
+ * Represents an express router.
+ */
 export default class Router {
   router
+
+  /**
+   * Creates a new instance of a router.
+   */
   constructor () {
     this.router = express.Router()
     this.#registerRoutes()
