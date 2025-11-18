bb15d71ca460c81aeff8730749845e011e54e250
Patricia Johansson <pat.joh.2025@gmail.com>
2025-11-18 11:46:38 +0100

    Add unit test for ToDoRepository

--- server/jest.config.js
+++ server/jest.config.js
@@ -5,5 +5,15 @@ const tsJestTransformCfg = createDefaultPreset().transform
 /** @type {import("jest").Config} **/
 export const testEnvironment = 'node'
 export const transform = {
-  ...tsJestTransformCfg
+  ...tsJestTransformCfg,
+  '^.+\\.tsx?$': [
+    'ts-jest',
+    {
+      tsconfig: './test/tsconfig.json',
+      useESM: true
+    }
+  ],
+  '\\.[jt]sx?$': ['ts-jest', { useESM: true }]
 }
+export const moduleNameMapper = { '(.+)\\.js': '$1' }
+export const extensionsToTreatAsEsm = ['.ts']
